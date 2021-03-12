787deb15f93d3e408b68f525dc52c7c1f5b07691
yujun-1982 <80206815+yujun-1982@users.noreply.github.com>
2021-03-12 19:42:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.2/g' package/base-files/files/bin/config_generate
-
-#2. 设置密码为空
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt /package/lean/default-setting/files/zzz-default-settings
