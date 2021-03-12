8b629dfe025a2b979e83883d292b9939f4c312fc
yujun-1982 <80206815+yujun-1982@users.noreply.github.com>
2021-03-12 18:17:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#Modify default IP
+sed -i 's/192.168.1.1/10.10.10.2/g' openwrt/package/base-files/files/bin/config_generate
